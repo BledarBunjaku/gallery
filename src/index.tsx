b34ecc2930e0fda536543e9ba43e96ba08638966
BledarBunjaku <bledarbunjaku@gmail.com>
2021-05-11 09:19:50 +0200

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Test from './appp';
-import Test1 from "./atest"
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Test from "./appp";
+import { Test1 } from "./container/test1";
+import { Test2 } from "./container/test2";
+import reportWebVitals from "./reportWebVitals";
+import { Tires } from "./container/tiers";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test1 />
+    <Tires />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
